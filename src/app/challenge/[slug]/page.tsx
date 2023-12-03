66054dc094de7c297436d202dcda7463e65c3307
Yan-pg <yancesa.07@gmail.com>
2023-12-03 12:18:09 -0300

    feat: add host on eviroments veriables

--- src/app/challenge/[slug]/page.tsx
+++ src/app/challenge/[slug]/page.tsx
@@ -1,8 +1,5 @@
-import { ProgressBar } from "@/modules/challenge/components/ProgressBar";
-import { Button } from "@/modules/share";
 import units from "@/data/units.json";
 import { generateSlug } from "@/tools";
-// import { ChallengeSentence } from "@/modules/challenge/components/ReadOrListen";
 import { ChallengeTemplate } from "@/modules/challenge/templete";
 
 export const dynamic = "force-dynamic";
@@ -18,11 +15,14 @@ export default async function Challenge({ params }: ChallengePageProps) {
 
   const unit = units.content.find((unit) => generateSlug(unit.title) === slug);
 
-  const response = await fetch("http://localhost:3000/api/generate-sentences", {
-    method: "POST",
-    body: JSON.stringify({ unit: unit?.contents }),
-    cache: "no-store",
-  });
+  const response = await fetch(
+    `${process.env.API_HOST}/api/generate-sentences`,
+    {
+      method: "POST",
+      body: JSON.stringify({ unit: unit?.contents }),
+      cache: "no-store",
+    }
+  );
 
   const sentences = await response.json();
 
